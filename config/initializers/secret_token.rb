255f2db2a1215027256b9107034ec043f613cd89
Howard Ding <hding2@illinois.edu>
2012-02-01 14:53:35 -0600

    Initial add of generated rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Etd::Application.config.secret_token = '81a5c348f67bdb622c567e29b495f220c42df703e92c4361d562f25a8d39e2771c175bc687e157f5a9416d061e3a08fc1d90b8c05fb55fbd8f4b2fc88be8f03e'
