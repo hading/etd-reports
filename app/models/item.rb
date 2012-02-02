1daca20cbc8b068d443864a5fc159b3b57ad7116
Howard Ding <hding2@illinois.edu>
2012-02-02 16:25:03 -0600

    make the qualifier argument optional as well

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,13 +20,13 @@ class Item < ActiveRecord::Base
     submission = self.vireo_submission
     Hash.new.tap do |h|
       h[:uin] = submission.uin || 'unknown'
-      h[:student_name] = finder.call('creator', nil)
+      h[:student_name] = finder.call('creator')
       h[:degree_type] = submission.degree_type || 'unknown'
       h[:degree_level] = finder.call('degree', 'level')
       h[:degree_name] = finder.call('degree', 'name')
       h[:degree_department] = finder.call('degree', 'department')
       h[:embargo_option] = submission.embargo_description || ""
-      h[:title] = finder.call('title', nil)
+      h[:title] = finder.call('title')
       h[:deposit_date] = submission.submission_date.strftime('%F')
       h[:degree_date] = finder.call('date', 'submitted')
       h[:advisor_name] = finder.call('contributor', 'advisor')
@@ -40,7 +40,7 @@ class Item < ActiveRecord::Base
   #return a lambda. Calling with just element and qualifier returns the first match.
   #Calling with anything in the third argument position returns an array with all matches
   def value_finder(triples)
-    lambda do |element, qualifier, multiple_values = nil|
+    lambda do |element, qualifier = nil, multiple_values = nil|
       values = triples.select {|t| t.element == element and t.qualifier == qualifier}.collect {|t| t.value}
       return multiple_values ?  values : (values.first || '')
     end
